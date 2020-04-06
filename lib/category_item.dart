c994a30c04875a676bdf4f6675a1b1b3c7013dec
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-04-06 22:27:22 +0000

    Add select category function
    use Navigation and Material Page Route

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(builder: (_) {
+        return CategoryMealsScreen();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () => selectCategory(context),
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(15),
       child: Container(
