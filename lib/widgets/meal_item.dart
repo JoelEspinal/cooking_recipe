c7debb039f460cf68f145186ea8cd92c5d338218
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-04-08 21:29:45 +0000

    Add basic structure to meal item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 
 class MealItem extends StatelessWidget {
+  void selectMeal() {}
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      onTap: selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
