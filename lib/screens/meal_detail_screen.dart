7f2278f486a0d27f4d3a1b8913f2e2080f165776
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-04-09 19:28:39 +0000

    Set Image in detail screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
 
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
       appBar: AppBar(
-        title: Text("The meail - $mealId"),
+        title: Text('${selectedMeal.title}'),
       ),
-      body: Center(
-        child: Text("The meail - $mealId"),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+        ],
       ),
     );
   }
