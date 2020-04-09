68d895568ebbda7e25dc2d43126613047956900a
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-04-09 19:55:58 +0000

    extract title widget to method

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -5,6 +5,16 @@ import '../dummy_data.dart';
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
 
+  Widget buildSectionTitle(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
@@ -25,10 +35,7 @@ class MealDetailScreen extends StatelessWidget {
           ),
           Container(
             margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredients',
-              style: Theme.of(context).textTheme.title,
-            ),
+            child: buildSectionTitle(context, 'Ingredients'),
           ),
           Container(
             decoration: BoxDecoration(
@@ -52,6 +59,7 @@ class MealDetailScreen extends StatelessWidget {
               itemCount: selectedMeal.ingredients.length,
             ),
           ),
+          buildSectionTitle(context, 'Setps'),
         ],
       ),
     );
