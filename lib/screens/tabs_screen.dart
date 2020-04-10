570eade6dbfe7de326e353c13aee07899e206d52
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-04-10 18:17:23 +0000

    Change to shifting navigation type

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -37,11 +37,18 @@ class _TabsSreenState extends State<TabsScreen> {
           unselectedItemColor: Colors.white,
           selectedItemColor: Theme.of(context).accentColor,
           currentIndex: _selectedPageIndex,
+          type: BottomNavigationBarType.shifting,
           items: [
             BottomNavigationBarItem(
-                icon: Icon(Icons.category), title: Text('Categories')),
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              title: Text('Categories'),
+            ),
             BottomNavigationBarItem(
-                icon: Icon(Icons.star), title: Text('Favorites'))
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              title: Text('Favorites'),
+            )
           ],
         ),
       ),
