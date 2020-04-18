7796be786e6af8d72c1ef4eed69b1e9833d7705e
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-04-18 21:43:55 +0000

    Receive a list of favorite meals

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../models/meal.dart';
+
 class FavoritesScreen extends StatelessWidget {
+  List<Meal> favoriteMeals = [];
+
+  FavoritesScreen(this.favoriteMeals);
+
   @override
   Widget build(BuildContext context) {
     return Center(child: Text('You have no favories yet - start adding some!'));
