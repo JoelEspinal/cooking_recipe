be323d187e7800c5aade8563df6ea355309e9abc
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-04-18 21:30:07 +0000

    Change default message

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Favories'));
+    return Center(child: Text('You have no favories yet - start adding some!'));
   }
 }
